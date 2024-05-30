8297a98cf1eea56a33bc75a7116aca26ddd9c8f5
SavithaSkr <savitha848410@gmail.com>
2024-05-30 09:20:00 -0400

    update project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Search from "./components/Search";
+import Products from "./components/Products";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Search></Search>
+      <Products></Products>
+      <About></About>
+      <Footer></Footer>
+    </>
+  );
+}
+export default App;
---
+++ src/components/Search.js
@@ -0,0 +1,9 @@
+//Search component
+function Search() {
+  return (
+    <div className="searchbar">
+      <input type="text" placeholder="Search"></input>
+    </div>
+  );
+}
+export default Search;
